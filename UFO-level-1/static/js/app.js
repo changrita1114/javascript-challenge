64cae438fd6a762ea64f705f95d2a90c269cff36
changrita1114 <hardheard101@gmail.com>
2020-11-30 14:14:24 -0800

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+let button = d3.select("#filter-btn");
+
+// Select the form
+let form = d3.select("#datetime");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  let inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  let inputValue = inputElement.property("value");
+  let filteredData = data.filter(item => item.datetime === inputValue);
+
+  // Append the filtered data in table
+  let tbody = d3.select("tbody");
+
+  filteredData.forEach((item) => {
+    let row = tbody.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+};
